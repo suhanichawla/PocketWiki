73c717632a791a832c800a9b9b6430cbc4427350
Suhani Chawla <suhanichawla2000@gmail.com>
2024-01-16 18:33:24 +0530

    article links added

--- app.py
+++ app.py
@@ -5,26 +5,28 @@ from components.search_bar import display_search_bar
 from components.article_display import display_article_results
 from domain.data_fetch import download_articles
 
+def get_save_articles(refresh=False):
+    if 'articles' not in st.session_state or 'article_links' not in st.session_state or refresh==True:
+        print("article collection started")
+        st.session_state.articles, st.session_state.article_links = download_articles(5)
+    return st.session_state.articles, st.session_state.article_links
+
 def main():
     st.title("Article Search App")
-
-    if 'articles' not in st.session_state:
-        print("article collection started")
-        st.session_state.articles = download_articles(10)
     
-    articles_dict = st.session_state.articles
+    articles_dict, article_links = get_save_articles()
     model = load_model()
     encoded_titles = encode_articles(model, articles_dict)
 
     if st.button("Refresh Articles"):
-        articles_dict = download_articles(5)  # Refresh the articles
+        articles_dict, article_links = get_save_articles(refresh=True)
         encoded_titles = encode_articles(model, articles_dict)
 
     query = display_search_bar()
 
     if query:
         results = search_articles(query, encoded_titles, articles_dict, model)
-        display_article_results(results)
+        display_article_results(results, article_links)
 
 if __name__ == "__main__":
     main()
--- domain/data_fetch.py
+++ domain/data_fetch.py
@@ -2,11 +2,14 @@ import requests
 
 def download_articles(n=200):
     articles = {}
+    article_links = {}
     for _ in range(n):
         response = requests.get("https://en.wikipedia.org/api/rest_v1/page/random/summary")
         if response.status_code == 200:
             data = response.json()
             title = data['title']
             content = data['extract']
-            articles[title] = content.lower()  # Convert to lowercase
-    return articles
+            url = data['content_urls']['desktop']['page']
+            articles[title] = content.lower()
+            article_links[title] = url
+    return articles, article_links
